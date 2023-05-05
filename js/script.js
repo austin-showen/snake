4aefb1fe457d90054704b716d742406aae6c9891
austin-showen <austin.showen@gmail.com>
2023-05-05 14:30:51 -0500

    create basic game logic

--- js/script.js
+++ js/script.js
@@ -15,6 +15,22 @@ const COLORS = {
   food: 'blue',
   cell: 'red'
 }
+const KEYCODES = {
+  38: 'u',
+  87: 'u',
+  39: 'r',
+  68: 'r',
+  40: 'd',
+  83: 'd',
+  37: 'l',
+  65: 'l'
+}
+const DIRECTIONS = {
+  u: [-1, 0],
+  r: [0, 1],
+  d: [1, 0],
+  l: [0, -1]
+}
 
 /* +-+-+-+- STATE VARIABLES -+-+-+-+ */
 let tailLength
@@ -24,6 +40,7 @@ let tail
 let foodLocation
 let highScore
 let alive
+let moveQueued
 
 /* +-+-+-+- HTML ELEMENTS -+-+-+-+ */
 const boardEl = document.querySelector('#board')
@@ -41,6 +58,7 @@ const createBoard = () => {
 }
 
 const newFood = () => {
+  if (foodLocation) clearCell(foodLocation)
   const foodRow = Math.floor(Math.random() * HEIGHT)
   const foodCol = Math.floor(Math.random() * WIDTH)
   foodLocation = [foodRow, foodCol]
@@ -65,12 +83,71 @@ const render = () => {
   renderSnake()
 }
 
+const clearCell = (loc) => {
+  const cellEl = document.querySelector(`#r${loc[0]}c${loc[1]}`)
+  cellEl.style.backgroundColor = COLORS.cell
+}
+
+const compareArrays = (arr1, arr2) => {
+  return arr1.join(' ') === arr2.join(' ')
+}
+
+const checkCollisions = (loc) => {
+  if (loc[0] < 0 || loc[0] >= HEIGHT || loc[1] < 0 || loc[1] >= WIDTH) {
+    alive = false
+  }
+  tail.forEach((cell) => {
+    if (compareArrays(cell, loc)) alive = false
+  })
+}
+
 const moveSnake = () => {
-  tail.push(currentLocation)
-  tail.unshift()
-  currentLocation[0] += currentDirection[0]
-  currentLocation[1] += currentDirection[1]
-  render()
+  moveQueued = false
+  tail.push([...currentLocation])
+  const newLocation = [
+    currentLocation[0] + currentDirection[0],
+    currentLocation[1] + currentDirection[1]
+  ]
+
+  checkCollisions(newLocation)
+  console.log(alive)
+
+  if (alive) {
+    currentLocation = newLocation
+
+    if (compareArrays(currentLocation, foodLocation)) {
+      tailLength++
+      newFood()
+    } else {
+      let prevTail = tail.shift()
+      clearCell(prevTail)
+    }
+    render()
+  }
+}
+
+const handleKeydown = (e) => {
+  if (moveQueued) return
+
+  const keyCodeStr = e.keyCode.toString()
+  const validKeys = Object.keys(KEYCODES)
+  if (!validKeys.includes(keyCodeStr)) return
+
+  const newDirection = DIRECTIONS[KEYCODES[keyCodeStr]]
+  if (
+    newDirection[0] + currentDirection[0] === 0 &&
+    newDirection[1] + currentDirection[1] === 0
+  )
+    return
+  else {
+    moveQueued = true
+    currentDirection = newDirection
+  }
+}
+
+const playGame = () => {
+  moveSnake()
+  if (alive) setTimeout(playGame, TICKSPEED)
 }
 
 const init = () => {
@@ -78,14 +155,16 @@ const init = () => {
   tailLength = 0
   tail = []
   currentLocation = [Math.floor(WIDTH / 2), Math.floor(HEIGHT / 2)]
-  currentDirection = [0, 1]
+  currentDirection = DIRECTIONS.r
   alive = true
   newFood()
   render()
-  moveSnake()
+  playGame()
 }
 
 /* +-+-+-+- EVENT LISTENERS -+-+-+-+ */
+
+document.addEventListener('keydown', handleKeydown)
 
 /* +-+-+-+- INITIATION -+-+-+-+ */
 
